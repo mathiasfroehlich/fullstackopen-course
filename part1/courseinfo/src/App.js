7a0b5d2bc334caa6daaa9cc9db710daec035ded1
Mathias Fröhlich <mathias.froehlich@r-software.at>
2022-11-24 15:00:46 +0100

    course information, step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,20 +1,52 @@
-const Hello = (props) => {
+const Header = (props) => {
   return (
-    <div>
-      <p>Hello {props.name}, your are {props.age} years old!</p>
-    </div>
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+} 
+
+const Content = (props) => {
+  const partsContent = props.parts.map((part, index) => {
+    return (
+      <Part key={index} name={part.name} exercises={part.exercises} />
+    )
+  })
+
+  return (
+    <p>
+      { partsContent }
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.totalExercises}</p>
   )
 }
 
 const App = () => {
-  const name = 'Mathias'
-  const age = 31
+  const course = 'Half Stack application development'
+  const parts = [
+    {name: 'Fundamentals of React', exercises: 10},
+    {name: 'Using props to pass data', exercises: 7},
+    {name: 'State of a component', exercises: 14}
+  ]
+  const totalExercises = parts.map(el => el.exercises).reduce((a, b) => a + b, 0)
 
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name={name} age={age} />
-    </>
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total totalExercises={totalExercises} />
+    </div>
   );
 }
 
